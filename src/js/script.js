cba92d5d1cb29d1ea44539744e117395a121c9e6
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-07-05 07:35:47 +0200

    Remove duplicated renderInMenu

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-  'use strict';
+  ('use strict');
 
   const select = {
     templateOf: {
@@ -45,16 +45,18 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    },
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    menuProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.menuProduct).innerHTML
+    ),
   };
 
-  class Product{
-    constructor(id, data){
-      const thisProduct=this;
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
 
       thisProduct.id = id;
       thisProduct.data = data;
@@ -63,46 +65,41 @@
 
       console.log('new Product:', thisProduct);
     }
-    renderInMenu(){
+
+    renderInMenu() {
       const thisProduct = this;
-    }
-  
-    renderInMenu(){
-    const thisProduct = this;
 
-    // generate HTML based on template
-const generatedHTML = templates.menuProduct(thisProduct.data);
-    //create element using utils.createElementFromHTML
-thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-    //find menu container
-const menuContainer = document.querySelector(select.containerOf.menu);
-    //add element to menu
-    menuContainer.appendChild(thisProduct.element);
-
-
+      // generate HTML based on template
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      //create element using utils.createElementFromHTML
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      //find menu container
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      //add element to menu
+      menuContainer.appendChild(thisProduct.element);
     }
   }
   const app = {
-    initMenu: function(){
+    initMenu: function () {
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
-      for(let productData in thisApp.data.products){
+      for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
     },
-    initData: function(){
+    initData: function () {
       const thisApp = this;
 
       thisApp.data = dataSource;
     },
-    init: function(){
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-      
+
       thisApp.initData();
       thisApp.initMenu();
     },
