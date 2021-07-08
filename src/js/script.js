039297769418b615c6879d0fca18930a6f491b65
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-07-08 18:27:24 +0200

    Add counting of price by choosing options in form

--- src/js/script.js
+++ src/js/script.js
@@ -67,7 +67,7 @@
       thisProduct.initOrderForm();
       thisProduct.processOrder();
 
-      console.log('new Product:', thisProduct);
+      //console.log('new Product:', thisProduct);
     }
 
     renderInMenu() {
@@ -92,11 +92,11 @@
       thisProduct.form = thisProduct.element.querySelector(
         select.menuProduct.form
       );
-      console.log('thisProduct.form', thisProduct.form);
+      //console.log('thisProduct.form', thisProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(
         select.all.formInputs
       );
-      console.log('thisProduct.formInputs', thisProduct.formInputs);
+      //console.log('thisProduct.formInputs', thisProduct.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(
         select.menuProduct.cartButton
       );
@@ -117,7 +117,7 @@
         const activeProduct = document.querySelector(
           select.all.menuProductsActive
         );
-        console.log('activeProduct', activeProduct);
+        //console.log('activeProduct', activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct && activeProduct != thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
@@ -131,37 +131,75 @@
 
     initOrderForm() {
       const thisProduct = this;
-      console.log('initOrderForm');
+      //console.log('initOrderForm');
 
       thisProduct.form.addEventListener('submit', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
-        console.log('Form Event Lintener added');
+        //console.log('Form Event Lintener added');
       });
 
       for (let input of thisProduct.formInputs) {
         input.addEventListener('change', function () {
           thisProduct.processOrder();
-          console.log('Process after change in form');
+          //console.log('Process after change in form');
         });
       }
 
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
-        console.log('Process after click');
+        //console.log('Process after click');
       });
     }
     processOrder() {
       const thisProduct = this;
       console.log('processOrder');
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for (let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if (option.default != 'true') {
+              // add option price to price variable
+              price += option.price;
+            } else {
+              // check if the option is default
+              if (option.default == 'true') {
+                // reduce price variable
+                price -= option.price;
+              }
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
   const app = {
     initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      //console.log('thisApp.data:', thisApp.data);
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
