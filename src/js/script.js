a34eb70a3c99e7376a56baf3df6befdac4661450
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-07-09 22:12:39 +0200

    Add counting of price by choosing options in form / part 2

--- src/js/script.js
+++ src/js/script.js
@@ -173,19 +173,22 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
-
+          let optionSelected =
+            formData[paramId] && formData[paramId].includes(optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          if (optionSelected) {
             // check if the option is not default
-            if (option.default != 'true') {
+            if (option.default != true) {
               // add option price to price variable
               price += option.price;
-            } else {
-              // check if the option is default
-              if (option.default == 'true') {
-                // reduce price variable
-                price -= option.price;
-              }
+              console.log('no-Default price added');
+            }
+          } else {
+            // check if the option is default
+            if (option.default == true) {
+              // reduce price variable
+              price -= option.price;
+              console.log('Default price added');
             }
           }
         }
