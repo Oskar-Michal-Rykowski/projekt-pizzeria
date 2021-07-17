eaa9b2d595880e225c7901632f1d40edde7394e2
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-07-17 13:40:59 +0200

    Correct function of counting value after mentors comments

--- src/js/script.js
+++ src/js/script.js
@@ -65,10 +65,9 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
-      thisProduct.initAmountWidget();
       thisProduct.processOrder();
 
-      ////console.log('new Product:', thisProduct);
+      //console.log('new Product:', thisProduct);
     }
 
     renderInMenu() {
@@ -93,11 +92,11 @@
       thisProduct.form = thisProduct.element.querySelector(
         select.menuProduct.form
       );
-      ////console.log('thisProduct.form', thisProduct.form);
+      //console.log('thisProduct.form', thisProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(
         select.all.formInputs
       );
-      ////console.log('thisProduct.formInputs', thisProduct.formInputs);
+      //console.log('thisProduct.formInputs', thisProduct.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(
         select.menuProduct.cartButton
       );
@@ -107,10 +106,6 @@
 
       thisProduct.imageWrapper = thisProduct.element.querySelector(
         select.menuProduct.imageWrapper
-      );
-
-      thisProduct.amountWidgetElem = thisProduct.element.querySelector(
-        select.menuProduct.amountWidget
       );
     }
 
@@ -126,7 +121,7 @@
         const activeProduct = document.querySelector(
           select.all.menuProductsActive
         );
-        ////console.log('activeProduct', activeProduct);
+        //console.log('activeProduct', activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct && activeProduct != thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
@@ -140,39 +135,33 @@
 
     initOrderForm() {
       const thisProduct = this;
-      ////console.log('initOrderForm');
+      //console.log('initOrderForm');
 
       thisProduct.form.addEventListener('submit', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
-        ////console.log('Form Event Lintener added');
+        //console.log('Form Event Lintener added');
       });
 
       for (let input of thisProduct.formInputs) {
         input.addEventListener('change', function () {
           thisProduct.processOrder();
-          ////console.log('Process after change in form');
+          //console.log('Process after change in form');
         });
       }
 
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
-        ////console.log('Process after click');
+        //console.log('Process after click');
       });
-    }
-
-    initAmountWidget() {
-      const thisProduct = this;
-
-      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
     }
     processOrder() {
       const thisProduct = this;
-      //console.log('processOrder');
+      console.log('processOrder');
       //What options has been choosen?
       const formData = utils.serializeFormToObject(thisProduct.form);
-      //console.log('formData', formData);
+      console.log('formData', formData);
 
       // Default price of the product with default options
       let price = thisProduct.data.price;
@@ -181,13 +170,13 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        //console.log(paramId, param);
+        console.log(paramId, param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          //console.log(optionId, option);
+          console.log(optionId, option);
           let optionSelected =
             formData[paramId] && formData[paramId].includes(optionId);
           // check if there is param with a name of paramId in formData and if it includes optionId
@@ -195,20 +184,19 @@
             '.' + paramId + '-' + optionId
           );
 
-          if (optionSelected && optionImg) {
-            optionImg.classList.add(classNames.menuProduct.imageVisible);
-          } else {
-            if (optionImg) {
+          if (optionImg) {
+            if (optionSelected)
+              optionImg.classList.add(classNames.menuProduct.imageVisible);
+            else {
               optionImg.classList.remove(classNames.menuProduct.imageVisible);
             }
+          }
+          if (optionSelected && !option.default) {
+            price += option.price;
+          }
 
-            if (optionSelected && option.default != true) {
-              price += option.price;
-            } else {
-              if (option.default == true) {
-                price -= option.price;
-              }
-            }
+          if (!optionSelected && option.default) {
+            price -= option.price;
           }
         }
 
@@ -217,7 +205,7 @@
       }
     }
   }
-
+  /*
   class AmountWidget {
     constructor(element) {
       const thisWidget = this;
@@ -237,7 +225,7 @@
       if (thisWidget.value !== newValue) {
         thisWidget.value = newValue;
       }
-    }
+    } 
 
     getElements(element) {
       const thisWidget = this;
@@ -253,12 +241,12 @@
         select.widgets.amount.linkIncrease
       );
     }
-  }
+  }*/
 
   const app = {
     initMenu: function () {
       const thisApp = this;
-      ////console.log('thisApp.data:', thisApp.data);
+      //console.log('thisApp.data:', thisApp.data);
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
@@ -270,11 +258,11 @@
     },
     init: function () {
       const thisApp = this;
-      //console.log('*** App starting ***');
-      //console.log('thisApp:', thisApp);
-      //console.log('classNames:', classNames);
-      //console.log('settings:', settings);
-      //console.log('templates:', templates);
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
